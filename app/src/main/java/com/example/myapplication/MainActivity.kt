90a2fee2dab913fe4572766a68a9054197c433c9
thinhvh <thinhvohuu@git.sohacorp.vn>
2021-06-04 21:46:33 +0700

    thinhvh sua code

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,6 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        // thinhvh
+        var a=10
     }
 }
