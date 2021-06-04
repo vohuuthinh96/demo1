b831881e4b8ed957646aa93ba68fe52a36c3bb58
thinhvh <thinhvohuu@git.sohacorp.vn>
2021-06-04 21:49:16 +0700

    thinhvh xy lu dang nhap

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +12,12 @@ class MainActivity : AppCompatActivity() {
         var a=10
 
         var b=20
+
+        //xử lý đăng nhập
+        xulydangnhap()
+    }
+
+    fun xulydangnhap(){
+        Log.d("thinh", "xulydangnhap: ")
     }
 }
